bd64297e559dab105882241056bba80bea010aa4
John <jlross90@gmail.com>
2020-09-25 13:32:19 +1000

    added algovis project

---
+++ projects/AlgoVis/precache-manifest.20722e49ff069f29e5025ba37a15138a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "525b9761e4dfeb02ca2dd3f825f534c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a604509e3d4439648a91",
+    "url": "/static/css/main.11471464.chunk.css"
+  },
+  {
+    "revision": "6360910438ee92283593",
+    "url": "/static/js/2.756ba945.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.756ba945.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a604509e3d4439648a91",
+    "url": "/static/js/main.fe8cae73.chunk.js"
+  },
+  {
+    "revision": "72c07dcb21740fe3cff4",
+    "url": "/static/js/runtime-main.b593c7cf.js"
+  }
+]);
